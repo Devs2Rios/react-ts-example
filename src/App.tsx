ee8564b75d7d278c0932114247b0c4173906d7c5
eduairet <hola@eduairet.com>
2023-08-16 20:17:17 -0600

    Starter project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Todos from './components/Todos';
+
+export default function App(): JSX.Element {
+    return (
+        <>
+            <h1>TSX</h1>
+            <p>Hi TypeScript</p>
+            <Todos />
+        </>
+    );
+}
